b208773ae819940d9758b12160cc6731af8e6ebf
jaswanthsngp <sanagapallijaswanth@gmail.com>
2023-07-30 12:36:22 +0530

    Codeforces Round 889 Div 2 Editorial

--- CF1855B.cpp
+++ CF1855B.cpp
@@ -1,6 +1,7 @@
 // https://codeforces.com/contest/1855/problem/B
 
-// pretests passed in the contest
+// pretests passed in the contest, then accepted.
+// thought that if the two consequent numbers share a multiple, those must be small
 
 #include <iostream>
 #include <cmath>
@@ -23,3 +24,24 @@ int main() {
         cout<<maxCnt<<endl;
     } 
 }
+
+// It works! However, here is the code from editorial.
+// he said, the first one to break the modulus is the limit
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int t;
+//     unsigned long long int n;
+//     cin>>t;
+//     while(t--) {
+//         cin>>n; int i;
+//         for(i=1; i<n && n%i==0; i++) {}
+//         if(n==i)
+//             cout<<i<<endl;
+//         else
+//             cout<<i-1<<endl;
+//     }
+//     return 0;
+// }
