9e654beb0c63c008960bfbaabe00e08d5c831a66
jaswanthsngp <sanagapallijaswanth@gmail.com>
2023-08-06 14:36:27 +0530

    Template to use for CodeChef and CodeForces Rounds

---
+++ template.cpp
@@ -0,0 +1,19 @@
+// https://example.com/url-to-the-problem
+
+// description
+
+#include <iostream>
+#include <limits>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    int t, n;
+    cin>>t;
+    while(t--) {
+        cin>>n;
+    }
+    return 0;
+}
